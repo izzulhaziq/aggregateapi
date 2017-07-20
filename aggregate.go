69e204b5ec3c33a2d63716390365e51b6467d5a8
Mohd Izzul Haziq <izzulhaziq@Mohds-MacBook-Pro.local>
2017-07-20 23:02:54 +0800

    handle malformed data and allow count aggr

--- aggregate.go
+++ aggregate.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"strings"
 	"time"
 
@@ -17,11 +16,7 @@ func aggregate(param aggrParam) <-chan map[string]interface{} {
 			panic(err)
 		}
 	}, 5).Map(func(data map[string]interface{}) flow.KeyValue {
-		key := groupKey(param.GroupBy, param.Interval, data)
-		val, ok := data[param.AggregatedField].(int)
-		if !ok {
-			val, _ = strconv.Atoi(data[param.AggregatedField].(string))
-		}
+		key, val := mapToGroup(param, data)
 		return flow.KeyValue{Key: key, Value: val}
 	}).ReduceByKey(func(x int, y int) int {
 		return x + y
@@ -49,16 +44,36 @@ func aggregate(param aggrParam) <-chan map[string]interface{} {
 	return aggrOut
 }
 
+func mapToGroup(param aggrParam, data map[string]interface{}) (string, int) {
+	key := getKey(param.GroupBy, param.Interval, data)
+	var val int
+	if param.AggregatedField == "" {
+		val = 1
+	} else {
+		parsed, ok := data[param.AggregatedField].(int)
+		if !ok {
+			parsed, _ = strconv.Atoi(data[param.AggregatedField].(string))
+		}
+		val = parsed
+	}
+
+	return key, val
+}
+
 func closeFlow() {
 	copy(flow.Contexts[0:], flow.Contexts[1:])
 	flow.Contexts[len(flow.Contexts)-1] = nil
 	flow.Contexts = flow.Contexts[:len(flow.Contexts)-1]
 }
 
-func groupKey(groupBy []string, interval string, data map[string]interface{}) (key string) {
+func getKey(groupBy []string, interval string, data map[string]interface{}) (key string) {
 	var keys []string
 	for _, g := range groupBy {
-		keys = append(keys, data[g].(string))
+		k, ok := data[g].(string)
+		if !ok {
+			k = "null"
+		}
+		keys = append(keys, k)
 	}
 
 	t, ok := data[cfg.dateKey].(time.Time)
@@ -79,9 +94,13 @@ func fromInterval(t time.Time, interval string) string {
 	var timeKey string
 	switch interval {
 	case "daily":
-		timeKey = fmt.Sprintf("%04d-%02d-%02d", t.Year(), t.Month(), t.Day())
+		timeKey = t.Format("2006-01-02")
+		//timeKey = fmt.Sprintf("%04d-%02d-%02d", t.Year(), t.Month(), t.Day())
 	case "monthly":
-		timeKey = fmt.Sprintf("%04d-%02d", t.Year(), t.Month())
+		timeKey = t.Format("2006-01")
+		//timeKey = fmt.Sprintf("%04d-%02d", t.Year(), t.Month())
+	case "yearly":
+		timeKey = t.Format("2006")
 	}
 	return timeKey
 }
