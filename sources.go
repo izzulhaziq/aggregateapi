d3a824148e1d5e99df723dcccd362bb7d2cba7a5
Mohd Izzul Haziq <izzulhaziq@Mohds-MacBook-Pro.local>
2017-07-23 15:51:50 +0800

    testing with yacr csv reader

--- sources.go
+++ sources.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"encoding/csv"
 	"fmt"
-	"io"
-	"log"
 	"math/rand"
 	"os"
 	"strconv"
-	"sync"
 	"time"
+
+	"github.com/gwenn/yacr"
 )
 
 type source interface {
@@ -28,47 +26,29 @@ func (c csvSource) read(out chan map[string]interface{}) error {
 	}
 	defer file.Close()
 
-	lineFeeds := make(chan []string)
-	var wg sync.WaitGroup
-	headers := []string{}
-	go func() {
-		lineCount := 0
-		reader := csv.NewReader(file)
-		for {
-			line, err := reader.Read()
-			if err == io.EOF {
-				break
-			} else if err != nil {
-				log.Printf("error reading record at line %d, %v", lineCount, err)
-				continue
+	var headers []string
+	lineCount := 0
+	reader := yacr.DefaultReader(file)
+	record := map[string]interface{}{}
+	i := 0
+	for reader.Scan() {
+		if lineCount == 0 {
+			headers = append(headers, reader.Text())
+			if reader.EndOfRecord() {
+				lineCount++
 			}
+		} else {
+			record[headers[i]] = reader.Text()
+			i++
 
-			if lineCount == 0 {
-				headers = append(headers, line...)
-			} else {
-				lineFeeds <- line
-
-			}
-			lineCount++
-		}
-		close(lineFeeds)
-	}()
-
-	wg.Add(cfg.concurRead)
-	for i := 0; i < cfg.concurRead; i++ {
-		go func() {
-			defer wg.Done()
-			for line := range lineFeeds {
-				record := map[string]interface{}{}
-				for i, v := range line {
-					record[headers[i]] = v
-				}
+			if reader.EndOfRecord() {
 				out <- record
+				i = 0
+				lineCount++
+				record = map[string]interface{}{}
 			}
-		}()
+		}
 	}
-
-	wg.Wait()
 	return nil
 }
 
